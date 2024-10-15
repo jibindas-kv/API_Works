e272f1d5eef55c3125200d292d2bb16aba9ff4fd
jibindaskv <jibindas.work@gmail.com>
2024-10-14 22:13:41 -0700

    first

--- lib/home.dart
+++ lib/home.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:simple_animated_button/elevated_layer_button.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -35,15 +35,16 @@ class _MyHomePageState extends State<MyHomePage> {
           preferredSize: Size.fromHeight(kToolbarHeight.r),
           child: AppBar(
             flexibleSpace: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [Colors.purple, Colors.blue],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight)),
+              decoration: BoxDecoration(color: Colors.black),
             ),
-            title: Text(
-              'Random Dog Images',
-              style: TextStyle(color: Colors.white),
+            title: Center(
+              child: Text(
+                'Random Dog Images',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 30),
+              ),
             ),
           )),
       body: Container(
@@ -51,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
         height: double.infinity.r,
         child: Column(
           children: [
-            20.verticalSpace,
+            70.verticalSpace,
             if (userData == null)
               Text(
                 'Press the Button',
@@ -61,29 +62,26 @@ class _MyHomePageState extends State<MyHomePage> {
               Stack(children: [
                 Center(
                   child: Container(
-                    width: 350.r,
-                    height: 340.r,
+                    width: 250.r,
+                    height: 240.r,
                     decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(200),
-                        gradient: LinearGradient(
-                            colors: [Colors.purple, Colors.blue],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight)),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(300),
+                    ),
                     child: Center(
                       child: Text(
-                        '🐶Loading...',
-                        style: TextStyle(color: Colors.white, fontSize: 30),
+                        'Loading...',
+                        style: TextStyle(color: Colors.black, fontSize: 30),
                       ),
                     ),
                   ),
                 ),
                 Center(
                   child: Padding(
-                    padding: const EdgeInsets.all(20.0).r,
+                    padding: const EdgeInsets.only(top: 5).r,
                     child: Container(
-                      width: 300.r,
-                      height: 300.r,
+                      width: 240.r,
+                      height: 230.r,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(150).r,
                           image: DecorationImage(
@@ -93,41 +91,61 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
               ]),
+
+
+            // 30.verticalSpace,
+            // Container(
+            //   width: 200.r,
+            //   height: 40.r,
+            //   decoration: BoxDecoration(
+            //       color: Colors.white,
+            //       borderRadius: BorderRadius.circular(30).r),
+            //   child: ElevatedButton(
+            //       style: ElevatedButton.styleFrom(
+            //           backgroundColor: Colors.white),
+            //       onPressed: () {
+            //         setState(() {
+            //           fetchImage();
+            //         });
+            //       },
+            //       child: Text(
+            //         'Next',
+            //         style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20),
+            //       )),
+            // ),
             30.verticalSpace,
-            Container(
-              width: 200.r,
-              height: 40.r,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [Colors.purple, Colors.blue],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight),
-                  borderRadius: BorderRadius.circular(30).r),
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent),
-                  onPressed: () {
-                    setState(() {
-                      fetchImage();
-                    });
-                  },
-                  child: Text(
-                    'Next',
-                    style: TextStyle(color: Colors.white),
-                  )),
+            ElevatedLayerButton(
+              onClick: () {
+                setState(() {
+                  fetchImage();
+                });
+              },
+              buttonHeight: 60,
+              buttonWidth: 200,
+              animationDuration: const Duration(milliseconds: 200),
+              animationCurve: Curves.ease,
+              topDecoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(),
+              ),
+              topLayerChild: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Next",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 10,),
+                  Icon(Icons.arrow_forward_sharp)
+                ],
+              ),
+              baseDecoration: BoxDecoration(
+                color: Colors.blue,
+                border: Border.all(),
+              ),
             ),
-            50.verticalSpace,
-            ShaderMask(
-                shaderCallback: (bounds) => LinearGradient(
-                            colors: [Colors.purple, Colors.blue],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight)
-                        .createShader(
-                      bounds,
-                    ),
-                child: Text('Dogs are Lovely Creatures',
-                    style: GoogleFonts.italiana(
-                        color: Colors.white, fontSize: 30))),
+            150.verticalSpace,
+            Text('"Random Dog Images\n      Using Dog Api"',
+                style: GoogleFonts.podkova(color: Colors.white, fontSize: 30)),
           ],
         ),
       ),
